7f3a22aef4f92f506698b752644ee5e779f96de7
JeremyRoalef <roalef944@morrisville.edu>
2025-05-25 14:44:46 -0400

    3.10 Creating Lobbies
    Turns out my game is connected to my college Unity account, not my personal account. The dashboard is for GameDev_Multiplayer (1). Everything is working properly, only the account is not my personal one.

--- GameDev_Multiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ GameDev_Multiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -5,23 +5,35 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using Unity.Services.Lobbies.Models;
 public class HostGameManager
 {
     //Note: no need for init method because the host is a client
 
     Allocation allocation;
     string joinCode;
+    string lobbyID;
+    
     const int MAX_CONNECTIONS = 20;
 
     const string GAME_SCENE_STRING = "GameplayScene";
 
     public async Task StartHostAsync()
     {
+        /*
+         * Order of hosting events:
+         *  1) Allocation setup
+         *  2) Retrieve join code
+         *  3) Transport setup
+         *  4) Lobby Setup
+         *  5) Start hosting
+         */
+
         //Allocation setup
         try
         {
@@ -33,7 +45,7 @@ public class HostGameManager
             return;
         }
 
-
+        //Get the join code
         try
         {
             joinCode = await Relay.Instance.GetJoinCodeAsync(allocation.AllocationId);
@@ -57,9 +69,57 @@ public class HostGameManager
         //Set the relay server data for our transport
         transport.SetRelayServerData(relayServerData);
 
+
+        //Set up the lobby
+        try
+        {
+            //Initialize lobby options
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                        )
+                }
+            };
+
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync(
+                "My Lobby", 
+                MAX_CONNECTIONS, 
+                lobbyOptions
+                );
+
+            lobbyID = lobby.Id;
+
+            //Tell the host instance to run this code's heartbeat coroutine
+            HostSingleton.Instance.StartCoroutine(HeartbeatLobby(15));
+        }
+        catch (LobbyServiceException lobbyServiceEx)
+        {
+            Debug.LogWarning($"{lobbyServiceEx.Message}");
+            return;
+        }
+
+
         //The start host button in the network manager game object
         NetworkManager.Singleton.StartHost();
 
         NetworkManager.Singleton.SceneManager.LoadScene(GAME_SCENE_STRING, LoadSceneMode.Single);
+    }
+
+    //Create lobby heartbeat (so it doesn't shut down)
+    IEnumerator HeartbeatLobby(float waitTimeSeconds)
+    {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+
+        while(true)
+        {
+            //Tell the lobby service to keep this lobby alive
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyID);
+            yield return delay;
+        }
     }
 }

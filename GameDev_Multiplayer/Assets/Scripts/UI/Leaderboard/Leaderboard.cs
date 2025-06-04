f376ded6b633b73fa305e558fde995cf0456d454
JeremyRoalef <roalef944@morrisville.edu>
2025-06-03 20:17:18 -0400

    4.7 Custom Data Types
    Key Takeaways:
    
    1) This section sets up the code for each player having its own line on the leaderboard. In the leaderboard script, a reference to the leaderboard entity holder and prefab are stored. Additionally, a network list is created, and this list requires that we create a custom struct to hold client information. this is set up in a new script called LeaderboardEntityState.
    
    2) The leaderboard entity state struct stores the client id, player name, and the number of coins that the player picked up, all of which are information that will be displayed in the leaderboard. To get this working with the network list in the leaderboard script, this struct needs to implement the INetworkSerialiable interface, which adds a single method called NetworkSerialize. In this, we pass the reference for each data type we wish to sync across the network using serializer.SerializeValue (ref myVariableName); Additionally, this struct needs to perform Boolean operations on itself, so it implements the IEquatable<LeaderboardEntityState> interface, giving the struct an Equals method where we check if all the values in the struct match.
    
    3) You cannot initialize a network list when declaring the list in the class. You need to initialize it in awake

--- GameDev_Multiplayer/Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ GameDev_Multiplayer/Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Leaderboard : MonoBehaviour
+public class Leaderboard : NetworkBehaviour
 {
+    [SerializeField]
+    Transform leaderboardEntityHolder;
 
+    [SerializeField]
+    LeaderboardEntityDisplay leaderboardEntityPrefab;
+
+    NetworkList<LeaderboardEntityState> leaderboardEntities;
+
+    private void Awake()
+    {
+        leaderboardEntities = new NetworkList<LeaderboardEntityState>();
+    }
 }
